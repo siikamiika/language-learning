9afddb1d62be7a24e9f55b044163c0df1d3a9887
siikamiika <siikamiika@users.noreply.github.com>
2019-05-15 02:06:36 +0300

    canto-parser: more cleanup

--- chinese/cantonese/dict/cantodict/cantodict-parse.py
+++ chinese/cantonese/dict/cantodict/cantodict-parse.py
@@ -9,14 +9,21 @@ import bs4
 CLEANUP = [
     (re.compile("^[\u200b\s]*"), ""),
     (re.compile("[\u200b\s]+"), " "),
+]
+
+CLEANUP_PINYIN = [
     # remove single TW pinyin that occurs before a PRC pinyin
     #                [tw pinyin]   [TW annotation        ][maybe /][PRC lookahead    ]
     (re.compile(r"\s?[a-z]+[1-6]\s*[\(\[](Taiwan|TW)[\)\]]\s*/?\s*(?=[a-z]+[1-6]\s*[\(\[])"), ""),
     # same but in reverse
     (re.compile(r"(?<=[\)\]])\s*/?\s*[a-z]+[1-6]\s*[\(\[](Taiwan|TW)[\)\]]"), ""),
+
     # remove entire TW pinyin blocks
     (re.compile(r"^.+?[\(\[](Taiwan|TW)[\)\]][,;]? "), ""),
     (re.compile(r"(?<=[\)\]])[,;]? .+?[\(\[](Taiwan|TW)[\)\]]"), ""),
+
+    # normalize
+    (re.compile("\s*;\s*"), "/"),
 ]
 
 def parse_page(soup):
@@ -24,10 +31,13 @@ def parse_page(soup):
     for row in table.find_all('tr')[1:-1]:
         data = row.find_all('td')
         cleaned_data = []
-        for d in data[:5]:
+        for i, d in enumerate(data[:5]):
             d = d.text.strip().replace('\t', ' ')
             for patt, repl in CLEANUP:
                 d = patt.sub(repl, d)
+            if i in [1, 2]:
+                for patt, repl in CLEANUP_PINYIN:
+                    d = patt.sub(repl, d)
             cleaned_data.append(d)
         yield tuple(cleaned_data) + ((data[0].find('a') or {'href': '/////'})['href'].split('/')[5],)
 
